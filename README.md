bb99c9007f42baccc292620ac45eaaa35d32e170
Umut <umut91c@gmail.com>
2021-01-03 21:56:49 +0100

    first version

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # AutoConcedeBot_LoR
 Auto-concede/win with a second account, Legends Of Runeterra
+
+Setup:
+1) Set your resolution in the script
+2) Set the boolean "first_winning_account" to True or vice verca for the second account
+3) Start script. Bot starts in 5 seconds. Make sure that your LoR Client is on screen 
+
+
+Note:
+-Make sure you start both scripts at the same time
+-Your resolution needs to have a ratio of 1.77 like for example 1920x1080 or 1366x768
+
+
---
+++ autoconcede_bot.py
@@ -0,0 +1,112 @@
+import numpy as np
+from enum import Enum
+from pynput.keyboard import Key, Controller as KeyboardController
+from pynput.mouse import Button, Controller as MouseController
+import time
+import random
+from pyautogui import press
+
+
+# Variables that you might have to change
+
+width= 1920
+height= 1080
+first_winning_account = True
+have_registered_riot_account = True
+
+# Init
+keyboard = KeyboardController()
+mouse = MouseController()
+init_waiting = True
+
+
+def clickMouse(x, y):
+    mouse.position = (x, y)
+    sleep(1,2)
+    mouse.click(Button.left, 1)
+
+def getCurrentMousePosition():
+    print(mouse.position)
+
+def applyRatio(ratio_x,ratio_y):
+    global width
+    global height
+    return (round(width*ratio_x),round(height*ratio_y))
+
+## Actuall Button Clicks
+
+def clickDeck():
+    mouse.position = applyRatio(0.34,0.29)
+    time.sleep(0.1)
+    mouse.click(Button.left, 1)
+
+def clickReady():
+    mouse.position = applyRatio(0.86,0.89)
+    time.sleep(0.1)
+    mouse.click(Button.left, 1)
+
+def clickSetting():
+    mouse.position = applyRatio(0.96,0.05)
+    time.sleep(0.1)
+    mouse.click(Button.left, 1)
+
+def clickSurrender():
+    mouse.position = applyRatio(0.43,0.83)
+    time.sleep(0.1)
+    mouse.click(Button.left, 1)
+
+def clickSurrenderNoRiot():
+    mouse.position = applyRatio(0.34,0.83)
+    time.sleep(0.1)
+    mouse.click(Button.left, 1)
+
+def clickOK(x, y):
+    mouse.position = applyRatio(0.57,0.56)
+    time.sleep(0.1)
+    mouse.click(Button.left, 1)
+
+def clickContinue(x, y):
+    mouse.position = applyRatio(0.66,0.90)
+    time.sleep(0.1)
+    mouse.click(Button.left, 1)
+
+
+
+time.sleep(5)
+counter=0
+while(True):
+    if(not first_winning_account):
+        clickDeck()
+        time.sleep(1)
+        clickReady()
+        time.sleep(28)
+        clickSetting()
+        time.sleep(1)
+        if(have_registered_riot_account):
+            clickSurrender()
+        else:
+            clickSurrenderNoRiot()
+        time.sleep(1)
+        clickOK()
+        time.sleep(15)
+        clickContinue()
+        time.sleep(3)
+        counter = counter + 1 
+    else: ## Loosing
+        clickDeck()
+        time.sleep(1)
+        clickReady()
+        time.sleep(28)
+        time.sleep(1)
+        time.sleep(1)
+        time.sleep(15)
+        clickContinue()
+        time.sleep(3)
+        counter = counter + 1 
+    if counter == 5:
+        first_winning_account = not first_winning_account
+    if counter == 10:
+        exit()
+
+
+cv2.waitKey(0)
